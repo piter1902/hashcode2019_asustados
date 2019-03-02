a06b9f046c00c07cd32dc81e0e501ceba2f57827
Pedro Tamargo <pedro.tamargo.allue@protonmail.com>
2019-03-02 13:17:13 +0100

    escritura lista

--- python/pic.py
+++ python/pic.py
@@ -1,13 +1,13 @@
 
 
 class Pic:
-    def __init__(self, orien: str, id: int, tags: set):
-        __id = id
-        __orien = orien
-        __tags = tags
+	def __init__(self, orien: str, id: int, tags: set):
+		__id = id
+		__orien = orien
+		__tags = tags
 
-    def tags(self):
-        return set(self.__tags)
+	def tags(self):
+		return set(self.__tags)
 
-    def id(self) -> int:
-        return self.__id
+	def id(self) -> int:
+		return self.__id
--- python/slide.py
+++ python/slide.py
@@ -2,27 +2,27 @@ from pic import Pic
 
 
 class Slide:
-    def __init__(self, pic: Pic):
-        __pic1 = pic
-        __pic2 = None
-        __tags = __pic1.tags()
+	def __init__(self, pic: Pic):
+		__pic1 = pic
+		__pic2 = None
+		__tags = __pic1.tags()
 
-    def __init__(self, pic1: Pic, pic2: Pic):
-        __pic1 = pic1
-        __pic2 = pic2
-        __tags = __pic1 | __pic2
+	def __init__(self, pic1: Pic, pic2: Pic):
+		__pic1 = pic1
+		__pic2 = pic2
+		__tags = __pic1 | __pic2
 
-    def tags(self):
-        return self.__tags
+	def tags(self):
+		return self.__tags
 
-    def min(self, s):
-        a1 = s.tags() & self.tags()
-        a2 = s.tags() - self.tags()
-        a3 = self.tags() - s.tags()
-        return min(len(a1), len(a2), len(a3))
+	def min(self, s):
+		a1 = s.tags() & self.tags()
+		a2 = s.tags() - self.tags()
+		a3 = self.tags() - s.tags()
+		return min(len(a1), len(a2), len(a3))
 
-    def __str__(self):
-        s = str(self.__pic1.id())
-        if self.__pic2 is not None:
-            s += str(self.__pic2.id())
-        return s
+	def __str__(self):
+		s = str(self.__pic1.id())
+		if self.__pic2 is not None:
+			s += str(self.__pic2.id())
+		return s
