029d81834219c36855e9037003b9814971c0e530
Nestor <nestormg98@gmail.com>
2019-03-02 14:07:18 +0100

    Peleandome con la insercion en listas
    (para añadir el identificador al principio de cada pic

--- python/main.py
+++ python/main.py
@@ -1,4 +1,28 @@
+import fileinput
+import pic
+
+def a_ver_esa_lectura(pics):
+	i = 0
+	for line in fileinput.input():
+		print(line)
+		separada = line.split(' ')		# Separamos por ' '
+		print(separada.insert(0, [i]))
+		pics.append(separada.insert(0, i)) # Le meto el id al principio
+		i+=1 # No me dejaba hacer i++  >:(
+
+
+# de "lista de listas de strings" a "lista de pics"
+def ll_to_lp(input, output):
+	for pic in input:
+		for atrib in pic:
+			a
+
 
 if __name__ == "__main__":
 	print ("Holi")
+	pics = []
+	a_ver_esa_lectura(pics)
+	for pic in pics:
+		print(pic)
+
 
